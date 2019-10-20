c7cdb2d0fa344c93c2c533cda448fcfc1c51b2d2
NKSchuurman <git@nkschuurman.com>
2019-10-20 17:31:31 +0200

    Added ICC option, made input conditional on ICC vs Std. Devs.

--- app.R
+++ app.R
@@ -12,18 +12,30 @@ ui <- fluidPage(
     column(2,
            wellPanel(
     
-    # Input: Between values
-    sliderInput(inputId="sdX_b", label="between sd X", min=0, max=10, value=1,step=.1),
-    sliderInput(inputId="sdY_b", label="between sd Y", min=0, max=10, value=1,step=.1),
+    radioButtons(inputId="ICCornot", label=NULL, choices=list("ICC", "Std. Dev."),inline=TRUE), 
+    
+    sliderInput(inputId="rho_w", label="within rho", min=-1, max=1, value=-0.5,step=.1),
     sliderInput(inputId="rho_b", label="between rho", min=-1, max=1, value=0.5,step=.1),
     
-    #Input: Within values
-    sliderInput(inputId="sdX_w", label="within sd X", min=0, max=10, value=1,step=.1),
-    sliderInput(inputId="sdY_w", label="within sd Y", min=0, max=10, value=1,step=.1),
-    sliderInput(inputId="rho_w", label="within rho", min=-1, max=1, value=-0.5,step=.1),
+    conditionalPanel(
+      condition = "input.ICCornot == 'ICC'",
+      # Input: ICCs
+      sliderInput(inputId="ICCX", label="ICC X", min=0, max=1, value=0.5,step=.1),
+      sliderInput(inputId="ICCY", label="ICC Y", min=0, max=1, value=0.5,step=.1)
+      ),
     
+    conditionalPanel(
+      condition = "input.ICCornot == 'Std. Dev.'",
+      # Input: Between sds
+      sliderInput(inputId="sdX_b", label="between sd X", min=0, max=10, value=1,step=.1),
+      sliderInput(inputId="sdY_b", label="between sd Y", min=0, max=10, value=1,step=.1),
+       #Input: Within sds
+      sliderInput(inputId="sdX_w", label="within sd X", min=0, max=10, value=1,step=.1),
+      sliderInput(inputId="sdY_w", label="within sd Y", min=0, max=10, value=1,step=.1)
+    ),
+   
     #Input: Plotting Options
-    sliderInput(inputId="np", label="N for plot", min=0, max=1000, value=500,step=100)
+    sliderInput(inputId="np", label="N for plot", min=100, max=1000, value=500,step=100)
     
            )       
     ),
@@ -58,7 +70,9 @@ server <- function(input, output) {
   
  ###Reactive functions
   
-  #Calculate grand standard deviation for X
+  ######for when input$ICCornot=="Std. Dev.######
+  
+    #Calculate grand standard deviation for X
  fsdX <- reactive({
     varX_b=input$sdX_b^2
     varX_w = input$sdX_w^2
@@ -66,8 +80,7 @@ server <- function(input, output) {
     sdX  = sqrt(varX)
     return(sdX)
   })
- 
- #Calculate grand standard deviation for Y
+  #Calculate grand standard deviation for Y
  fsdY <- reactive({
    varY_b = input$sdY_b^2 
    varY_w = input$sdY_w^2
@@ -76,6 +89,21 @@ server <- function(input, output) {
    return(sdY)
  })
  
+##########################################
+   
+    frho_c <- reactive({
+      
+      if(input$ICCornot=="ICC"){
+      rho_c = input$ICCX * input$ICCY * input$rho_b + (1-input$ICCX) * (1-input$ICCY) * input$rho_w
+      return(rho_c)
+      }
+      if(input$ICCornot=="Std. Dev."){
+        rho_c = input$sdX_b/fsdX() * input$sdY_b/fsdY() * input$rho_b + input$sdX_w/fsdX() * input$sdY_w/fsdY() * input$rho_w
+        return(rho_c)
+      }
+      
+   })
+
  fsimdat_b <- reactive({
    simdat_b = mvrnorm(n=input$np,mu=c(0,0), Sigma=matrix(c(1,input$rho_b,input$rho_b,1),2,2))
    return(simdat_b)
@@ -84,11 +112,6 @@ server <- function(input, output) {
  fsimdat_w <- reactive({
    simdat_w = mvrnorm(n=input$np,mu=c(0,0), Sigma=matrix(c(1,input$rho_w,input$rho_w,1),2,2))
    return(simdat_w)
- })
- 
- frho_c <- reactive({
-   rho_c = input$sdX_b/fsdX() * input$sdY_b/fsdY() * input$rho_b + input$sdX_w/fsdX() * input$sdY_w/fsdY() * input$rho_w
-   return(rho_c)
  })
  
  fsimdat_c <- reactive({
@@ -107,17 +130,17 @@ server <- function(input, output) {
  
  #Between plot
   output$plot_b <- renderPlot({
-    plot_b=plot(fsimdat_b(),pch=19, ylim=c(-3,3),xlim=c(-3,3),col=col_b, xlab="X between", ylab="Y between", main=paste("Between rho =", input$rho_b))
+    plot_b=plot(fsimdat_b(),pch=19, ylim=c(-3,3),xlim=c(-3,3),col=col_b, xlab="X between (standardized)", ylab="Y between (standardized)", main=paste("Between rho =", input$rho_b))
   })
  
   #Within plot 
   output$plot_w <- renderPlot({
-    plot_w=plot(fsimdat_w(),pch=19, ylim=c(-3,3),xlim=c(-3,3), col=col_w, xlab="X within", ylab="Y within", main=paste("Within rho =", input$rho_w) )
+    plot_w=plot(fsimdat_w(),pch=19, ylim=c(-3,3),xlim=c(-3,3), col=col_w, xlab="X within (standardized)", ylab="Y within (standardized)", main=paste("Within rho =", input$rho_w) )
   })
   
   #Cross-sectional plot
   output$plot_c <- renderPlot({
-    plot_c=plot(fsimdat_c(),pch=19, ylim=c(-3,3),xlim=c(-3,3),col=col_c[fcol_c_select()], xlab="X cross-sectional", ylab="Y cross-sectional", main=paste("Cross-sectional rho =", round(frho_c(),2)) )
+    plot_c=plot(fsimdat_c(),pch=19, ylim=c(-3,3),xlim=c(-3,3),col=col_c[fcol_c_select()], xlab="X cross-sectional (standardized)", ylab="Y cross-sectional (standardized)", main=paste("Cross-sectional rho =", round(frho_c(),2)) )
   })
   
 }
